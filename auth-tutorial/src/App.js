e1e4f60deb6f080122c339faba9a3a3b4126c5d8
geraldcraig <geraldcraig@me.com>
2026-04-19 13:03:39 +0100

    completed

--- auth-tutorial/src/App.js
+++ auth-tutorial/src/App.js
@@ -4,23 +4,34 @@ import { BrowserRouter, Route, Routes } from 'react-router';
 import Dashboard from './components/Dashboard';
 import Login from "./components/Login";
 import Preferences from './components/Preferences';
+import ProtectedRoute from './components/ProtectedRoute';
 import useToken from './components/useToken';
 
 function App() {
-
     const { token, setToken } = useToken();
-
-    if(!token) {
-        return <Login setToken={setToken} />
-    }
 
     return (
         <div className="wrapper">
             <h1>Application</h1>
             <BrowserRouter>
                 <Routes>
-                    <Route path="/dashboard" element={<Dashboard />} />
-                    <Route path="/preferences" element={<Preferences />} />
+                    <Route path="/login" element={<Login setToken={setToken} />} />
+                    <Route
+                        path="/dashboard"
+                        element={
+                            <ProtectedRoute token={token}>
+                                <Dashboard />
+                            </ProtectedRoute>
+                        }
+                    />
+                    <Route
+                        path="/preferences"
+                        element={
+                            <ProtectedRoute token={token}>
+                                <Preferences />
+                            </ProtectedRoute>
+                        }
+                    />
                 </Routes>
             </BrowserRouter>
         </div>
---
+++ auth-tutorial/src/components/ProtectedRoute.js
@@ -0,0 +1,9 @@
+import { Navigate } from 'react-router';
+
+export default function ProtectedRoute({ children, token }) {
+    if (!token) {
+        return <Navigate to="/login" replace />;
+    }
+
+    return children;
+}
